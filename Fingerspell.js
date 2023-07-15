5569446c8d9a6ba1570ed5d24e2aa1563624edb3
programmingpixels <danny@programmingpixels.com>
2023-07-15 18:19:57 +0100

    WIP

---
+++ Fingerspell.js
@@ -0,0 +1,5 @@
+import { fingerspellingWordlist } from "./fingerspelling/wordlist.js";
+
+
+//https://stackoverflow.com/questions/49338193/how-to-use-code-from-script-with-type-module
+window.startFingerspelling = function startFingerspelling() {...
