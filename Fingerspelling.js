99cb439ff573a3741ad0b6d42a97019f85ccf26f
programmingpixels <danny@programmingpixels.com>
2024-10-02 19:25:56 +0100

    Add new input box for favourite letters

--- Fingerspelling.js
+++ Fingerspelling.js
@@ -18,11 +18,45 @@ window.main = function main() {
 var randomWord = ""
 
 function setNewWord() {
-    randomWord = fingerspellingWordlist[Math.floor(Math.random() * fingerspellingWordlist.length)];
+    favouriteLettersEle.value = favouriteLettersEle.value.toLowerCase().replace(/\s/g, "")
+
+    const favouriteLetters = favouriteLettersEle.value
+    var topCandidate = fingerspellingWordlist[Math.floor(Math.random() * fingerspellingWordlist.length)]
+    var topScore = scoreWord(favouriteLetters, topCandidate)
+
+    if (favouriteLetters.length > 0) {
+        for (let index = 0; index < 20; index++) {
+            // score candidate words, and replace if new word is better
+            var newCandidate = fingerspellingWordlist[Math.floor(Math.random() * fingerspellingWordlist.length)]
+            var newCandidateScore = scoreWord(favouriteLetters, newCandidate)
+            if (newCandidateScore > topScore) {
+                topCandidate = newCandidate
+                topScore = newCandidateScore
+            }
+        }
+    }
+
+    randomWord = topCandidate
+}
+
+function scoreWord(favouriteLetters, newCandidate) {
+    var score = 0
+    if (favouriteLetters.length == 0) {
+        return score
+    }
+    Array.from(favouriteLetters).forEach(favouriteLetter => {
+        Array.from(newCandidate).forEach(candidateLetter => {
+            if (candidateLetter == favouriteLetter) {
+                score++
+            }
+        })
+    })
+    return score
 }
 
 // get a few elements we need to use
 const answerboxEle = document.getElementById('answerbox')
+const favouriteLettersEle = document.getElementById('favouriteLetters')
 const imgEle = document.getElementById('imageLetter')
 
 window.fingerspellWord = async function fingerspellWord() {
@@ -30,24 +64,28 @@ window.fingerspellWord = async function fingerspellWord() {
 
     // disable answer box
     answerboxEle.disabled = true
+    await sleep(50);
 
     for (const letter of randomWord) {
+        imgEle.src = "fingerspelling/" + letter + ".webp"
+        await sleep(timeBetweenSigns * 1000);
+
         // blank out the image so that double letters are more obvious
         imgEle.src = ""
         await sleep(50);
-
-        imgEle.src = "fingerspelling/" + letter + ".webp"
-        await sleep(timeBetweenSigns * 1000);
     }
 
     // reenable text box
+    imgEle.src = "fingerspelling/finishflag.webp"
     answerboxEle.disabled = false
+
+    await sleep(50)  // make sure dom is settled before trying to focus element
+
     answerboxEle.focus()
 }
 
 window.answerboxUpdated = function answerboxUpdated() {
     // when the correct answer is input, generate a new word and show it
-    console.log(answerboxEle.value)
     if (answerboxEle.value.toLowerCase() === randomWord) {
         answerboxEle.value = "";
         setNewWord()
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # bsl-random-word
 
 You can start a local server with `python3 -m http.server 8000`
+Or more likely `python3 -m http.server 80` / `python -m http.server 80` depending on your setup
 It doesn't require any particular virtualenv.
 
 ## Adding words
